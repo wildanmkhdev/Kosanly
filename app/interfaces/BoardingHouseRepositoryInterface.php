2e3a2a06daf46805d880760ca3c3f9dc4368e102
wildanmkhdev <wildankhalidun97@gmail.com>
2025-08-24 20:57:05 +0700

    make provider

---
+++ app/interfaces/BoardingHouseRepositoryInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\interfaces;
+
+interface BoardingHouseRepositoryInterface
+{
+  public function getAllBoardingHouses($search = null, $city = null, $category = null);
+  public function getPopularBoardingHouses($limit = 5);
+  public function getBoardingHouseByCitySLug($slug);
+  public function getBoardingHouseByCategorySLug($slug);
+  public function getBoardingHouseBySLug($slug);
+}
