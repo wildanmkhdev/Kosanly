71ce7a8707291cd7f437664c464dccf06c54dc78
wildanmkhdev <wildankhalidun97@gmail.com>
2025-09-05 21:47:30 +0700

    push

---
+++ config/midtrans.php
@@ -0,0 +1,7 @@
+<?php
+return [
+  'serverKey' => env('MIDTRANS_SEVER_KEY', ''),
+  'isProduction' => env('MIDTRANS_IS_PRODUCTION', false),
+  'isSanitized' => env('MIDTRANS_IS_SANITIZED', true),
+  'is3ds' => env('MIDTRANS_IS_3DS', true),
+];
