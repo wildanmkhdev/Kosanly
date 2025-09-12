6294d9870324cecb8fd770b312eaa41a97abdd0d
wildanmkhdev <wildankhalidun97@gmail.com>
2025-09-13 00:00:57 +0700

    add push

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,10 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap"
         rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-    <link href="{{ asset('assets/output.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/input.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/output.css') }}" rel="stylesheet">
+    {{-- <link rel="stylesheet" href="{{ asset('assets/style.css') }}"> --}}
+
 </head>
 
 <body>
